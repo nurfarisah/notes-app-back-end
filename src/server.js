8f173247bac60289aece5a4c89a7172517ae8b50
Nur Farisah Patimah <nurfarisah149@gmail.com>
2023-03-05 17:30:09 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,24 @@
+// http://notesapp-v1.dicodingacademy.com/
+// chrome://flags/#block-insecure-private-network-requests
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
